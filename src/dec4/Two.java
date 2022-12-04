b8a6060d5f27e4631af8002dcea2470cbc81941e
Kristjan Jonsson <pe1178jo-s@student.lu.se>
2022-12-04 18:04:43 +0100

    blööö

--- src/dec4/Two.java
+++ src/dec4/Two.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-public class One {
+public class Two {
 
     public static void main(String[] args) throws FileNotFoundException {
         Scanner s = new Scanner(new File("./src/dec4/input.txt"), "UTF-8");
@@ -14,17 +14,18 @@ public class One {
         String[] first;
         String[] last;
         int count = 0;
-
+        
         try {
             while ((line=s.nextLine()) != null) {
                 sect = line.split(",");
                 first = sect[0].split("-");
                 last = sect[1].split("-");
 
-                if ((Integer.parseInt(first[0]) <= Integer.parseInt(last[0]) && Integer.parseInt(first[1]) >= Integer.parseInt(last[1])) ||
-                (Integer.parseInt(last[0]) <= Integer.parseInt(first[0]) && Integer.parseInt(last[1]) >= Integer.parseInt(first[1]))) {
+                if (Integer.parseInt(last[0]) <= Integer.parseInt(first[1]) && Integer.parseInt(last[0]) >= Integer.parseInt(first[0]) ||
+                Integer.parseInt(first[0]) <= Integer.parseInt(last[1]) && Integer.parseInt(first[0]) >= Integer.parseInt(last[0])) {
                     count++;
                 }
+
             }
         } catch (NoSuchElementException e) {
             System.out.println(count);
