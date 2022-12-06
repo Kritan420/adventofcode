1db23a2d00e084527c3ce14bdc112f16484b29ee
Kristjan Jonsson <pe1178jo-s@student.lu.se>
2022-12-06 11:05:52 +0100

    tunin

---
+++ src/dec6/tunaOne.java
@@ -0,0 +1,37 @@
+package dec6;
+
+import java.io.*;
+import java.util.*;
+
+public class tunaOne {
+    public static void main(String[] args) throws IOException {
+
+        Scanner s = new Scanner(new File("./src/dec6/input.txt"));
+        String stream = s.next();
+
+        Map<Character, Integer> marker = new HashMap<>();
+        int size = 0;
+       
+        int mark = 3;
+        int pos = 0;
+
+        while (size < 4 && mark < 4095) {
+
+            marker.clear();
+
+            for (int i = 0; i < 4; i++) {
+                marker.put(stream.charAt(pos+i), mark);
+            }
+
+            size = marker.size();
+
+            mark++;
+            pos++;
+            
+        }
+
+        System.out.println(mark);
+        
+        }
+
+    }
---
+++ src/dec6/tunaTwo.java
@@ -0,0 +1,37 @@
+package dec6;
+
+import java.io.*;
+import java.util.*;
+
+public class tunaTwo {
+    public static void main(String[] args) throws IOException {
+
+        Scanner s = new Scanner(new File("./src/dec6/input.txt"));
+        String stream = s.next();
+
+        Map<Character, Integer> marker = new HashMap<>();
+        int size = 0;
+       
+        int mark = 13;
+        int pos = 0;
+
+        while (size < 14 && mark < 4095) {
+
+            marker.clear();
+
+            for (int i = 0; i < 14; i++) {
+                marker.put(stream.charAt(pos+i), mark);
+            }
+
+            size = marker.size();
+
+            mark++;
+            pos++;
+            
+        }
+
+        System.out.println(mark);
+        
+        }
+
+    }
