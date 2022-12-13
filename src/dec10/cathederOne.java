dfa96c05a467bdc7272b9362c2e6c536c3fbfa87
Kristjan Jonsson <pe1178jo-s@student.lu.se~>
2022-12-13 19:24:43 +0100

    wutwutwutwut

--- src/dec10/cathederOne.java
+++ src/dec10/cathederOne.java
@@ -6,37 +6,43 @@ import java.util.*;
 public class cathederOne {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner sc = new Scanner(new File("./src/dec10/input2.txt"));
-
         String line = null;
         int cyc = 1;
+        int currC = 0;
         int x = 1;
         int save = 20;
         ArrayList<Integer> saved = new ArrayList<>();
 
         while (sc.hasNextLine()) {
+
             line = sc.nextLine();
+
+            if (currC >= save) {
+                save += 40;
+                saved.add(cyc*x);
+            }  
+
+
             if (line.startsWith("noop")) {
-                cyc += 1;
-                if (cyc >= save) {
-                    save += 40;
-                    saved.add(x*(cyc-1));
-                }
-            } else if (line.startsWith("addx")) {
-                cyc += 2;
-                
-                if (cyc >= save) {
-                    save += 40;
-                    saved.add(x*(cyc-2));
-                }
-                x += Integer.parseInt(line.substring(5));
+                cyc++;
             }
-            System.out.println(cyc + " " + x );
+            if (line.startsWith("addx")) {
+                x += Integer.parseInt(line.substring(5));
+                cyc += 2;
+            }
+
+
+
+            currC = cyc;            
         }
+
+
+
         int tot = 0;
         for (int c : saved) {
             tot += c;
         }
-        System.out.println(saved.get(0));
+        System.out.println(saved.get(4));
         System.out.println(tot);
     }
 }
